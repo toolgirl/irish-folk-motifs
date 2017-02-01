b4cbc63c1210e726cee1ee0e75fdea8e3f2e6bbf
Zia Rauwolf <toolgirl-github@tentacle.net>
2017-02-01 14:02:08 -0800

    Changed run.py

--- src/run.py
+++ src/run.py
@@ -37,10 +37,6 @@ reel_verification = shuffled[5214:7822]
 reel_test = shuffled[-2608:]
 print "There are {} empty cells.".format(reel_train['abc'].isnull().sum())
 
-
-
-
-
 # tunefr = TuneFragment({'mode': 'G', 'meter': '6/8', 'abc': '|:GE|D2B BAG|BdB A2B|GED G2A|B2B AGE|\rD2B BAG|BdB A2B|GED G2A|BGE G:|\rBd|e2e edB|ege edB|d2B def|gfe dBA|\rG2A B2d|ege d2B|AGE G2A|BGE G:|'})
 # tunefr.play()
 
@@ -54,32 +50,32 @@ print "There are {} empty cells.".format(reel_train['abc'].isnull().sum())
 # em.grid_search(reel_verification['abc'])
 # print("--- %s seconds ---" % (time.time() - start_time))
 
-# def make_ensembles():
-#     ensemble_output = {}
-#     for i in range(1, 7):
-#         start_time = time.time()
-#         em = EnsembleModel(n=i)
-#         em.fit_sub_models(reel_train['abc'])
-#         em.construct_grid_weights()
-#         print("--- %s seconds ---" % (time.time() - start_time))
-#         print "I'm grid searching"
-#         em.grid_search(reel_verification['abc'])
-#         ensemble_output[em.weights] = em.cumulative_score[em.weights]
-#         print("--- %s seconds ---" % (time.time() - start_time))
-#         # with open('model_'+str(i)+'.pkl', 'w') as f:
-#         #     for model in em.models:
-#         #         pickle.dump(model, f)
-#
-#     return ensemble_output
-#
-# results = make_ensembles()
-# df = pd.DataFrame.from_dict(results.items())
-# df.columns = ['weights', 'score']
-# ns = [len(X) for X in results.keys() ]
-# df['n']=ns
-# df.sort('n',ascending=True,inplace=True)
-# df.to_csv('results.csv', index=False)
-# plt.scatter(df.n.values.tolist(),df.score.values.tolist() )
+def make_ensembles():
+    ensemble_output = {}
+    for i in range(1, 3):
+        start_time = time.time()
+        em = EnsembleModel(n=i)
+        em.fit_sub_models(reel_train['abc'])
+        em.construct_grid_weights()
+        print("--- %s seconds ---" % (time.time() - start_time))
+        print "I'm grid searching"
+        em.grid_search(reel_verification['abc'])
+        ensemble_output[em.weights] = em.cumulative_score[em.weights]
+        print("--- %s seconds ---" % (time.time() - start_time))
+        # with open('model_'+str(i)+'.pkl', 'w') as f:
+        #     for model in em.models:
+        #         pickle.dump(model, f)
+
+    return ensemble_output
+
+results = make_ensembles()
+df = pd.DataFrame.from_dict(results.items())
+df.columns = ['weights', 'score']
+ns = [len(X) for X in results.keys() ]
+df['n']=ns
+df.sort('n',ascending=True,inplace=True)
+df.to_csv('results.csv', index=False)
+plt.scatter(df.n.values.tolist(),df.score.values.tolist() )
 
 
 

9594b6ad2431a63c723d6a65f30c9acc6990b716
Zia Rauwolf <toolgirl-github@tentacle.net>
2017-02-04 14:23:38 -0800

    Document NGramModel class.

--- src/n_gram.py
+++ src/n_gram.py
@@ -5,7 +5,6 @@ from math import log
 import string
 
 
-
 class NGramModel(object):
     """
     Takes a series from a DataFrame and an n and trains a model of a given n.
@@ -13,17 +12,25 @@ class NGramModel(object):
     Parameters
     ----------
     n : int
-        Size of the window to look at my data.
+        Size of the window to of characters to look at.
+    frequencies : dict of dicts, optional
+        Contains frequencies if the models is being instantiated from json.
+    n_gram_count : dict, optional
+        Contains counts of all the n_grams if the model is being instantiated from json.
 
     Attributes
     ----------
+    n : int
+        Size of the window of characters to consider in the string.
     token_count : defaultdict of Counter()
         Counts the occurance of the last character in a window to the preceding ones.
     frequencies : defaultdict of defaultdicts.
-        Holds the frequency a given last character shows up after the preceding ones given that all chararters show up.
-    perplexity : float
-        Holds the overall perplexity of my model given a new piece of data.
-
+        Holds the frequency a given last character shows up after the preceding ones
+        given that all chararters show up.
+    score : float
+        Pseudo probability of a token given its history.
+    n_gram_count : str
+        Count of a window of size n. This can give me the most frequent combinations of sounds.
     """
     def __init__(self, n, frequencies=None, n_gram_count=None):
         self.n = n
@@ -31,7 +38,7 @@ class NGramModel(object):
         self.token_count = None
         # defaultdict of defaultdict that holds the frequencies of a given suffix.
         if frequencies is not None:
-            self.convert_frequencies(frequencies)
+            self._convert_to_defaultdict(frequencies)
         else:
             self.frequencies = None
         self.score = None
@@ -40,25 +47,10 @@ class NGramModel(object):
         else:
             self.n_gram_count = None
 
-
-    #Start reading at the token and get the history of it.
-    def get_window_properties(self, tune, i):
-        window = tune[i - self.n + 1:i + 1]
-        history = window[:-1]
-        token = window[-1]
-        return history, token
-
-    def convert_frequencies(self, frequencies):
-        if self.n == 1:
-            self.frequencies = defaultdict(float, frequencies)
-        else:
-            self.frequencies = defaultdict(lambda: defaultdict(float))
-            for history, token in frequencies.iteritems():
-                self.frequencies[history] = defaultdict(float, token)
-
-
+    # Create dictionary of Counter() with the n-gram as key and the
+    # follwing next letter as key and the count of that occuring as
+    # value.
     def fit(self, series):
-        # For one character at a time.
         if self.n == 1:
             self.token_count = Counter()
             for tune in series.values:
@@ -67,12 +59,8 @@ class NGramModel(object):
                 self.token_count += Counter(tune)
             self.n_gram_count = self.token_count
             self._create_frequency()
-
         # for more than 1
         if self.n > 1:
-            # Create dictionary of Counter() with the n-gram as key and the
-            # follwing next letter as key and the count of that occuring as
-            # value.
             self.token_count = defaultdict(lambda: Counter())
             self.n_gram_count = Counter()
             # Go through each tune.
@@ -86,9 +74,26 @@ class NGramModel(object):
                     self.n_gram_count[ngram] += 1
             self._create_frequency()
 
-    # This is to make sure the string has the right amount of history.
+    # Returns the  psydo probability and perplexity of a given model.
+    def perplexity_score(self, new_tune):
+        sum_of_log_probs = 0
+        for i in xrange(self.n - 1, len(working_tune)):
+            history, token = self.get_window_properties(working_tune, i)
+            probability = self.frequencies[history][token]
+            sum_of_log_probs += log(probability + 1e-10)
+        perplexity = 1 - sum_of_log_probs
+        self.score = sum_of_log_probs
+        return perplexity, sum_of_log_probs
+
+    # Start reading at the token and get the history of it.
+    def get_window_properties(self, tune, i):
+        window = tune[i - self.n + 1:i + 1]
+        history = window[:-1]
+        token = window[-1]
+        return history, token
+
+    # This ensures a string has n-1 amount of history.
     def _pad_string(self, tune):
-        #pad with n-1 times
         pad = '?' * (self.n - 1)
         tune = pad + tune + '?'
         return tune
@@ -99,38 +104,31 @@ class NGramModel(object):
         tune = tune.replace(" ", "")
         return tune
 
-    # Calculates the frequency of a given n-gram.
+    # Calculates the frequency of a token by deviding this token by
+    # the number of tokens that follow a given history. Updates self.frequencies.
     def _create_frequency(self):
         n_total = 0
-        #for n size 1 its simpler and faster.
+        # Special case for n == 1.
         if self.n == 1:
             self.frequencies = defaultdict(float)
             total = float(sum(self.token_count.values()))
-            #predict method.
             frequencies = {k: v/total for k, v in self.token_count.iteritems()}
             self.frequencies.update(frequencies)
-
-        if self.n >1:
-            # Create a dictionary of frequencies that calculates the
-            # occurance of a letter after an n-gram and returns the
-            # frequency of that. Update self.frequencies
+        if self.n > 1:
             total_n = 0
             self.frequencies = defaultdict(lambda: defaultdict(float))
             for given, counter in self.token_count.iteritems():
                 total_n = float(sum(counter.values()))
                 self.frequencies[given] = defaultdict(float, {k: v/total_n for k, v in counter.iteritems()})
 
-
-    # Returns the sum_of_log_probs and perplexity of a given model.
-    def perplexity_score(self, new_tune):
-        sum_of_log_probs = 0
-        for i in xrange(self.n - 1, len(working_tune)):
-            history, token = self.get_window_properties(working_tune, i)
-            probability = self.frequencies[history][token]
-            sum_of_log_probs += log(probability + 1e-10)
-        perplexity = 1 - sum_of_log_probs
-        self.score = sum_of_log_probs
-        return perplexity, sum_of_log_probs
+        # Converts frequencies if handed in at instatiation to a defaultdict.
+        def _convert_to_defaultdict(self, frequencies):
+            if self.n == 1:
+                self.frequencies = defaultdict(float, frequencies)
+            else:
+                self.frequencies = defaultdict(lambda: defaultdict(float))
+                for history, token in frequencies.iteritems():
+                    self.frequencies[history] = defaultdict(float, token)
 
     # Returns the 10 most common n_grams.
     @property
